24c68fa89a08068774620180c4dbae18c3e2eb24
Sokun Chorn (The Blockchain Guy) <chornsokun@gmail.com>
2024-01-07 13:34:22 +1100

    note

--- mix.exs
+++ mix.exs
@@ -62,7 +62,7 @@ defmodule HelloKeycloak.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "assets.setup", "assets.build"],
+      setup: ["deps.get", "assets.setup", "heroicons 2.1.1", "assets.build"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
       "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
